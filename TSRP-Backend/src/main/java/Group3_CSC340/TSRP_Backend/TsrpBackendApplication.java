ee62d2ef6d1e214b6fcedf1e55c2669c073f4deb
William Sanders <wmsanders@uncg.edu>
2024-11-05 17:19:01 -0500

    backend

---
+++ TSRP-Backend/src/main/java/Group3_CSC340/TSRP_Backend/TsrpBackendApplication.java
@@ -0,0 +1,13 @@
+package Group3_CSC340.TSRP_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsrpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsrpBackendApplication.class, args);
+	}
+
+}
---
+++ TSRP-Backend/src/test/java/Group3_CSC340/TSRP_Backend/TsrpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Group3_CSC340.TSRP_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsrpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
