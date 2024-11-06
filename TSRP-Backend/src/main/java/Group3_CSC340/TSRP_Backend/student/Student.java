4b077a2f0c85e18e96398c2cd8cc6258b215bf6c
JayIsler14 <157390822+JayIsler14@users.noreply.github.com>
2024-11-06 18:06:49 -0500

    Update Student.java

--- TSRP-Backend/src/main/java/Group3_CSC340/TSRP_Backend/student/Student.java
+++ TSRP-Backend/src/main/java/Group3_CSC340/TSRP_Backend/student/Student.java
@@ -1,4 +1,94 @@
 package Group3_CSC340.TSRP_Backend.student;
 
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Table(name ="students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int s_id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profileDescription;
+
+    public Student(int s_id, String firstName, String lastName, String email, String profileDescription) {
+        this.s_id = s_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.profileDescription = profileDescription;
+    }
+
+    public Student(String firstName, String lastName, String email, String profileDescription) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.profileDescription = profileDescription;
+    }
+
+    public Student() {
+
+    }
+
+    public int getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(int s_Id) {
+        this.s_id = s_Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
 }
