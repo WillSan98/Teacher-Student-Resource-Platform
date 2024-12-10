efbd5095b41c11bc959de34035a1557c4b4783d9
JayIsler14 <157390822+JayIsler14@users.noreply.github.com>
2024-12-10 13:54:54 -0500

    Update StudentRepository.java

--- TSRP-Backend/src/main/java/Group3_CSC340/TSRP_Backend/student/StudentRepository.java
+++ TSRP-Backend/src/main/java/Group3_CSC340/TSRP_Backend/student/StudentRepository.java
@@ -1,8 +1,9 @@
 package Group3_CSC340.TSRP_Backend.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
